1af70c7719a599223be97c8ecf3c443bb378b633
Clearave <lqb153105@gmail.com>
2018-09-11 00:39:34 +0800

    UI优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -32,7 +32,16 @@ Page({
           userId: '1'
         }
       }
-    ]
+    ],
+    testImg: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3537273527,3254803069&fm=26&gp=0.jpg"
   },
-  onLoad: function() {}
+  onLoad: function() {},
+  viewImage: function (event) {
+    var currentSrc = event.currentTarget.dataset.src;
+    console.log("data is " + currentSrc);
+    wx.previewImage({
+      current: '', // 当前显示图片的http链接
+      urls: [currentSrc] // 需要预览的图片http链接列表
+    })
+  }
 })
