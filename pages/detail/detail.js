863e2786bf84bb010d9376312405f1d37591523e
Clearave <lqb153105@gmail.com>
2018-09-25 23:14:53 +0800

    detail页面的发送UI
    即将进行UI整改，适配 iPad

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,5 +1,6 @@
 // pages/detail/detail.js
 
+const app = getApp()
 const util = require('../../utils/util.js')
 
 Page({
@@ -10,13 +11,45 @@ Page({
   data: {
     testImg: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3537273527,3254803069&fm=26&gp=0.jpg",
     webpCode: '!/format/webp',
-    anonymousAvatarUrl: 'https://cloud-minapp-20256.cloud.ifanrusercontent.com/1g4MkWmjmCtMquLU.png'
+    anonymousAvatarUrl: 'https://cloud-minapp-20256.cloud.ifanrusercontent.com/1g4MkWmjmCtMquLU.png',
+    text: '',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    ishidename: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (option) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
     var currentId = option.id;
     this.setData({
       currentId: currentId
